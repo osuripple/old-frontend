01f84aef6a58f14f690e6e29e7ba055dca106737
Howl <the@howl.moe>
2016-05-27 15:29:18 +0200

    Use disqus for comments on blog posts

--- blog/themes/default/article.php
+++ blog/themes/default/article.php
@@ -8,61 +8,28 @@
 
 			<section class="footnote">
 				<!-- Unfortunately, CSS means everything's got to be inline. -->
-				<p>This article is my <?php echo numeral(article_number(article_id()), true); ?> oldest. It is <?php echo count_words(article_markdown()); ?> words long<?php if (comments_open()): ?>, and it’s got <?php echo total_comments().pluralise(total_comments(), ' comment'); ?> for now.<?php
-endif; ?> <?php echo article_custom_field('attribution'); ?></p>
+				<p>This article is my <?php echo numeral(article_number(article_id()), true); ?> oldest. It is <?php echo count_words(article_markdown()); ?> words long. <?php echo article_custom_field('attribution'); ?></p>
 			</section>
 		</section>
 
-		<?php if (comments_open()): ?>
 		<section class="comments">
-			<?php if (has_comments()): ?>
-			<ul class="commentlist">
-				<?php $i = 0;
-		while (comments()):
-			$i++; ?>
-				<li class="comment" id="comment-<?php echo comment_id(); ?>">
-					<div class="wrap">
-						<h2><?php echo comment_name(); ?></h2>
-						<time><?php echo relative_time(comment_time()); ?></time>
+			<div id="disqus_thread" style="max-width: 750px; margin-left: auto; margin-right: auto;"></div>
+			<script>
+			var disqus_config = function () {
+				this.page.url = '<?php echo full_url().current_url(); ?>';
+				this.page.identifier = 'blog_article_<?php echo article_id(); ?>';
+				this.page.title = '<?php echo page_title(); ?>';
+			};
+			(function() { // DON'T EDIT BELOW THIS LINE
+			var d = document, s = d.createElement('script');
 
-						<div class="content">
-							<?php echo htmlspecialchars(comment_text()); ?>
-						</div>
+			s.src = '//rppl.disqus.com/embed.js';
 
-						<span class="counter"><?php echo $i; ?></span>
-					</div>
-				</li>
-				<?php
-		endwhile; ?>
-			</ul>
-			<?php
-	endif; ?>
-
-			<form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
-				<?php echo comment_form_notifications(); ?>
-
-				<p class="name">
-					<label for="name">Your name:</label>
-					<?php echo comment_form_input_name('placeholder="Your name"'); ?>
-				</p>
-
-				<p class="email">
-					<label for="email">Your email address:</label>
-					<?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
-				</p>
-
-				<p class="textarea">
-					<label for="text">Your comment:</label>
-					<?php echo comment_form_input_text('placeholder="Your comment"'); ?>
-				</p>
-
-				<p class="submit">
-					<?php echo comment_form_button(); ?>
-				</p>
-			</form>
-
+			s.setAttribute('data-timestamp', +new Date());
+			(d.head || d.body).appendChild(s);
+			})();
+			</script>
+			<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 		</section>
-		<?php
-endif; ?>
 
 <?php theme_include('footer'); ?>
