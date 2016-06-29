a7da2a12b4e42f2066c6f88502a404db7a6eb6f8
avail <avail@pomf.se>
2016-06-30 00:44:26 +0200

    multi-account warning goes to the cm channel

--- inc/helpers/Schiavo.php
+++ inc/helpers/Schiavo.php
@@ -5,9 +5,10 @@ class Schiavo {
 	 * Sends a message to the channel #bunker.
 	 *
 	 * @param string $message
+     * @param int $cm
 	 */
-	static function Bunk($message) {
+	static function Bunk($message, $cm = 0) {
 		//if ($_SERVER["REMOTE_ADDR"] != "127.0.0.1")
-			return file_get_contents("http://zxq.co:31155/bunk?message=" . urlencode("**old-frontend** " . $message));
+			return file_get_contents("http://zxq.co:31155/bunk?message=" . urlencode("**old-frontend** " . $message) . ($cm != 0 ? "&chan=182600210206294026" : ""));
 	}
 }
