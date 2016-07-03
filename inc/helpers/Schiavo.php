5b9a955de8d68bfb10252ff6dad56947f9b3263e
Howl <the@howl.moe>
2016-07-03 17:19:15 +0200

    Fix avail's fuckup

--- inc/helpers/Schiavo.php
+++ inc/helpers/Schiavo.php
@@ -1,13 +1,26 @@
 <?php
 
 class Schiavo {
+	const BaseURL = "http://zxq.co:31155/";
+
 	/**
 	 * Sends a message to the channel #bunker.
 	 *
 	 * @param string $message
 	 */
 	static function Bunk($message) {
-		//if ($_SERVER["REMOTE_ADDR"] != "127.0.0.1")
-			return file_get_contents("http://zxq.co:31155/bunk?message=" . urlencode("**old-frontend** " . $message));
+		return __schiavoCall("bunk", $message);		
 	}
+	
+	static function CM($message) {
+		return __schiavoCall("cm", $message);
+	}
+}
+
+// Can a static function call a private function in the same class?
+// I don't remember. And don't wanna test to figure it out. So fuck it.
+function __schiavoCall($c, $message) {
+	//if ($_SERVER["REMOTE_ADDR"] != "127.0.0.1")
+		return file_get_contents(Schiavo::BaseURL . $c . "?message=" . urlencode("**old-frontend** " . $message));
+
 }
