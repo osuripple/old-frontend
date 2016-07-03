f347aa760b900bc831ddf9fc69e9b6302a0b6e09
Howl <the@howl.moe>
2016-07-03 18:45:32 +0200

    implement pagination in My API Applications

--- inc/pages/MyAPIApplications.php
+++ inc/pages/MyAPIApplications.php
@@ -6,8 +6,10 @@ class MyAPIApplications {
 	const LoggedIn = true;
 
 	public function P() {
+		$p = (int) @$_GET["page"];
+		if ($p < 0) $p = 0;
 		$myApps = $GLOBALS['db']
-			->fetchAll("SELECT id, name FROM api_applications WHERE owner = ?", [$_SESSION['userid']]);
+			->fetchAll("SELECT id, name FROM api_applications WHERE owner = ? LIMIT " . $p * 50 . ",50", [$_SESSION['userid']]);
 		?>
 		<div id="narrow-content" style="width:500px">
 			<h1><i class="fa fa-plane"></i> My API applications</h1>
@@ -15,7 +17,7 @@ class MyAPIApplications {
 			<p><a href="https://en.wikipedia.org/wiki/Application_programming_interface">API (Wikipedia)</a> | <a href="https://git.zxq.co/ripple/api-docs/wiki">Ripple API documentation</a></p>
 			<?php
 			if (!$myApps) {
-				echo '<b>Looks like you don\'t have any API application! (yet!)</b>';
+				echo '<b>Looks like you don\'t have any API applications! (yet!)</b>';
 			} else {
 				echo '<ul style="text-align:left;">';
 				foreach ($myApps as $app) {
@@ -23,6 +25,11 @@ class MyAPIApplications {
 				}
 				echo '</ul>';
 			}
+			?><?php 
+			$els = [];
+			if ($p > 0) $els[] = '<a href="index.php?p=32&page=' . ($p - 1) . '">Previous page</a>';
+			if (count($myApps) >= 50) $els[] = '<a href="index.php?p=32&page=' . ($p + 1) . '">Next page</a>';
+			if (count($els) > 0) echo '<br><br>' . implode(' | ', $els);
 			?><br><br>
 			<a href="submit.php?action=myApplications"><button type="button" class="btn btn-primary">New application</button></a>
 		</div>
