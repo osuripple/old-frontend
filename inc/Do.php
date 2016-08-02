e83b948bc794cd9ef773c64384e9152034cb5f2b
avail <avail@pomf.se>
2016-08-03 00:10:01 +0200

    this thing

--- inc/Do.php
+++ inc/Do.php
@@ -1353,7 +1353,8 @@ class D {
 
 				$msg = "[http://m.zxq.co/" . $bsid . ".osz " . $bm["song_name"] . "] is now ranked!";
 				$to = "#announce";
-				$resp = getJsonCurl(urlencode($URL["server"] . "/api/v1/fokabotMessage?k=" . $ScoresConfig["api_key"] . "&to=" . $to . "&msg=" . $msg));
+				$requesturl = urlencode($URL["server"] . "/api/v1/fokabotMessage?k=" . $ScoresConfig["api_key"] . "&to=" . $to . "&msg=" . $msg)
+				$resp = getJsonCurl($requesturl);
 
 				if ($resp["message"] != "ok") {
 					rapLog("Failed to send FokaBot message :( err: " . var_dump($resp["message"]));
